f97e0108a3e5f5ec6d1ac18d1cc62b0d9da69bb6
sunchangchen <sunchangchen@fang.com>
2018-04-18 11:28:13 +0800

    service-a测试阿波罗代码

---
+++ service-api-b/src/main/java/com/shizir/clouddemo/service/apiB/Controler/ApolloConfigControler.java
@@ -0,0 +1,37 @@
+package com.shizir.clouddemo.service.apiB.Controler;
+
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApolloConfigControler {
+
+    @Value("${URL.AgentCloud}")
+    String agentCloudUrl;
+
+    @Value("${IP.Redis}")
+    String redisIP;
+
+    @Value("${PassportInterfaceUrl:}")
+    String passportInterfaceUrl;
+
+    @RequestMapping("/ApolloConfig/AgentCloudUrl")
+    public String GetCongigAgentCloudUrl()
+    {
+        return agentCloudUrl;
+    }
+
+    @RequestMapping("/ApolloConfig/RedisIP")
+    public String GetCongigRedisIP()
+    {
+        return redisIP;
+    }
+
+    @RequestMapping("/ApolloConfig/common/psurl")
+    public String GetPassUrl()
+    {
+        return passportInterfaceUrl;
+    }
+}
