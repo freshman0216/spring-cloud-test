f7ea629c6d908db9b302fe36d742e181fc1de089
sunchangchen <sunchangchen@fang.com>
2018-05-02 11:30:59 +0800

    测试bean初始化后属性值是否跟随变化--不变化

--- service-api-b/src/main/java/com/shizir/clouddemo/service/apiB/config/ApolloBean.java
+++ service-api-b/src/main/java/com/shizir/clouddemo/service/apiB/config/ApolloBean.java
@@ -2,16 +2,35 @@ package com.shizir.clouddemo.service.apiB.config;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Date;
+
 public class ApolloBean {
 
-    public @Value("${URL.AgentCloud:}")
-    String agentCloudUrl;
+    @Value("${URL.AgentCloud:}")
+    public String agentCloudUrl;
 
     @Value("${IP.Redis:}")
     public String redisIP;
 
+    @Value("#{T(java.lang.Math).random()}")
+    public String random;
+
     public ApolloBean()
     {
-        System.out.println("ApolloBean init ...");
+        System.out.println("ApolloBean called ...");
+    }
+
+    @PostConstruct
+    private void initApolloBean()
+    {
+        System.out.println("initApolloBean called ...");
+    }
+
+    @PreDestroy
+    private void preDestoryApolloBean()
+    {
+        System.out.println("preDestoryApolloBean called ...");
     }
 }
--- service-api-b/src/main/java/com/shizir/clouddemo/service/apiB/config/TestBean.java
+++ service-api-b/src/main/java/com/shizir/clouddemo/service/apiB/config/TestBean.java
@@ -1,14 +1,26 @@
 package com.shizir.clouddemo.service.apiB.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Date;
 
 public class TestBean {
     public Integer hashCode;
-    public Date date;
 
-    TestBean()
+    @Value("#{T(java.lang.Math).random()}")
+    public String random;
+
+    @PostConstruct
+    private void initTestBean()
     {
-        date = new Date();
-        System.out.println("TestBean init ...");
+        System.out.println("initTestBean called ...");
+    }
+
+    @PreDestroy
+    private void preDestoryTestBean()
+    {
+        System.out.println("preDestoryTestBean called ...");
     }
 }
